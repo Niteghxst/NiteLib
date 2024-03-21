5bbd7f39c66c87d1d4c2758ee6af55ce5b50c4d0
Niteghxst <niteghxst@hotmail.com>
2024-03-21 05:32:35 +0100

    V0.1 Initial Upload

---
+++ src/org/niteghxst/api/util/NebulaeTypes.java
@@ -0,0 +1,16 @@
+package org.niteghxst.api.util;
+
+
+/**
+ * @author NITEGHXST
+ *
+ * Copyright 2024 Nite Corp
+ */
+public class NebulaeTypes {
+    /**
+     * Nebulae
+     */
+    public static final String NEBULA_CENTER_OLD = "nebula_center_old";
+    public static final String NEBULA_CENTER_AVERAGE = "nebula_center_average";
+    public static final String NEBULA_CENTER_YOUNG = "nebula_center_young";
+}
---
+++ src/org/niteghxst/api/util/SpecialSystemEntityTypes.java
@@ -0,0 +1,30 @@
+package org.niteghxst.api.util;
+
+
+/**
+ * @author NITEGHXST
+ *
+ * Copyright 2024 Nite Corp
+ */
+public class SpecialSystemEntityTypes {
+    // ╔═════════════════════════════════╗
+    // ║         System Entities         ║
+    // ╚═════════════════════════════════╝
+    /**
+     * Stellar Mirror & -Shade
+     */
+    public static final String STELLAR_MIRROR = "stellar_mirror";
+    public static final String STELLAR_SHADE = "stellar_shade";
+    /**
+     * Derelict "Station" Types
+     */
+    public static final String DERELICT_MOTHERSHIP = "derelict_mothership";
+    public static final String DERELICT_PROBE = "derelict_probe";
+    public static final String DERELICT_SURVEY_SHIP = "derelict_survey_ship";
+    public static final String DERELICT_STATION_SPORESHIP = "station_sporeship_derelict";
+    /**
+     * Other "Station" Types
+     */
+    public static final String CORONAL_SHUNT2 = "coronal_tap"; // coronal_shunt2
+    public static final String CRYOSLEEPER = "derelict_cryosleeper";
+}
